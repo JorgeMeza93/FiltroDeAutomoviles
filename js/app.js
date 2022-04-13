74cdbc015d71ed1c4def279c67ed984f30c5c9a4
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-04-12 22:55:55 -0500

    Listeners y objeto de busqueda listos

--- js/app.js
+++ js/app.js
@@ -1,14 +1,48 @@
 //Variables
 const resultados = document.querySelector("#resultado");
+const marca = document.querySelector("#marca");
+const maximo = document.querySelector("maximo");
+const minimo = document.querySelector("#minimo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
 const year = document.querySelector("#year");
 const max = new Date().getFullYear();
 const min = max - 10;
 
+//Objeto de busqueda
+const datosBusqueda = {
+    marca:  "",
+    year: "",
+    maximo: "" ,
+    minimo: "",
+    puertas: "",
+    transmision: "",
+    color: ""
+}
 
 document.addEventListener("DOMContentLoaded", ()=>{
     mostrarAutos();
     llenarSelect();
 });
+marca.addEventListener("change", (e)=>{
+    datosBusqueda.marca = e.target.value;
+});
+maximo.addEventListener("change", (e)=>{
+    datosBusqueda.maximo = e.target.value;
+});
+minimo.addEventListener("change", (e)=>{
+    datosBusqueda.minimo = e.target.value;
+});
+puertas.addEventListener("change", (e)=>{
+    datosBusqueda.puertas = e.target.value;
+});
+transmision.addEventListener("change", (e)=>{
+    datosBusqueda.transmision = e.target.value;
+});
+color.addEventListener("change", (e)=>{
+    datosBusqueda.color = e.target.value;
+});
+
 function mostrarAutos(){
     autos.forEach( auto =>{
         const {marca, modelo, year, precio, puertas, color, transmision} = auto
@@ -27,4 +61,5 @@ function llenarSelect(){
         option.innerText = i;
         year.appendChild(option);    
     }
+
 }
