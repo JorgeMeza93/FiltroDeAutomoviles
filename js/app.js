6f14fbe226de22c87da2a524aa31638ab5edd4d5
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-04-13 22:46:58 -0500

    Filtrado completado en todos los campos

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 //Variables
 const resultados = document.querySelector("#resultado");
 const marca = document.querySelector("#marca");
-const maximo = document.querySelector("maximo");
+const maximo = document.querySelector("#maximo");
 const minimo = document.querySelector("#minimo");
 const puertas = document.querySelector("#puertas");
 const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
 const year = document.querySelector("#year");
 const max = new Date().getFullYear();
 const min = max - 10;
@@ -17,35 +18,45 @@ const datosBusqueda = {
     minimo: "",
     puertas: "",
     transmision: "",
-    color: ""
+    color: "",
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    mostrarAutos();
+    mostrarAutos(autos);
     llenarSelect();
 });
 marca.addEventListener("change", (e)=>{
     datosBusqueda.marca = e.target.value;
     filtrarAuto();
 });
+year.addEventListener("change", (e)=>{
+    datosBusqueda.year = e.target.value;
+    filtrarAuto();
+});
 maximo.addEventListener("change", (e)=>{
     datosBusqueda.maximo = e.target.value;
+    filtrarAuto();  
 });
 minimo.addEventListener("change", (e)=>{
     datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
 });
 puertas.addEventListener("change", (e)=>{
     datosBusqueda.puertas = e.target.value;
+    filtrarAuto();  
 });
 transmision.addEventListener("change", (e)=>{
     datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
 });
 color.addEventListener("change", (e)=>{
     datosBusqueda.color = e.target.value;
+    filtrarAuto();
 });
 
-function mostrarAutos(){
-    autos.forEach( auto =>{
+function mostrarAutos(arrayAutos){
+    limpiarHTML();
+    arrayAutos.forEach( auto =>{
         const {marca, modelo, year, precio, puertas, color, transmision} = auto
         const autoHTML = document.createElement("p");
         autoHTML.classList.add("efecto-auto");
@@ -54,6 +65,11 @@ function mostrarAutos(){
         `;
         resultados.appendChild(autoHTML);
     })
+}
+function limpiarHTML(){
+    while(resultados.firstChild){
+        resultados.removeChild(resultados.firstChild);
+    }
 }
 function llenarSelect(){
     for(let i = max;i>=min;i--){
@@ -65,5 +81,56 @@ function llenarSelect(){
 
 }
 function filtrarAuto(){
-    
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter( filtrarMinimo).filter( filtrarMaximo ).filter( filtrarPuertas).filter( filtrarTransmision ).filter( filtrarColor )
+    console.log(resultado);
+    mostrarAutos(resultado);
+}
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+    if(marca){
+        return auto.marca === marca;
+    }
+    return auto;
+}
+function filtrarYear(auto){
+    const {year} = datosBusqueda;
+    if(year){
+        return auto.year === parseInt(year);
+    }
+    return auto;
+}
+function filtrarMinimo(auto){
+    const {minimo} = datosBusqueda;
+    if(minimo){
+        return auto.precio >= parseInt(minimo);
+    }
+    return auto;
+}
+function filtrarMaximo(auto){
+    const {maximo} = datosBusqueda;
+    if(maximo){
+        return auto.precio <= parseInt(maximo);
+    }
+    return auto;
+}
+function filtrarPuertas(auto){
+    const {puertas} = datosBusqueda;
+    if(puertas){
+        return auto.puertas === parseInt(puertas);
+    }
+    return auto
+}
+function filtrarTransmision(auto){
+    const {transmision} = datosBusqueda;
+    if(transmision){
+        return auto.transmision === transmision;
+    }
+    return auto;
+}
+function filtrarColor(auto){
+    const {color} = datosBusqueda;
+    if(color){
+        return auto.color === color;
+    }
+    return auto;
 }
